9632648275d0abfa93e894abc0137cacd8339ca3
Maryam Bano <maryambano.official@gmail.com>
2026-01-02 13:16:43 +0500

    Add average calculator script with function
    Introduces a Python script to calculate the average of three numbers. The script uses a function to prompt user input and prints the result, repeating the process for five users.

---
+++ Self-Learning-Data-Analytics/Python/Day 2/01-index.py
@@ -0,0 +1,21 @@
+""" 
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+average = (a+b+c)/3
+print(f"The average of 3 numbers: {a}, {b}, and {c} is {average}")
+"""
+
+# now making function
+def avg():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    c = int(input("Enter third number: "))
+
+    average = (a+b+c)/3
+    print(f"The average of 3 numbers: {a}, {b}, and {c} is {average}")
+
+for i in range(5):
+    print(f"Average Calculator for user {i+1}")
+    avg()
