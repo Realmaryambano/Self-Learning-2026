125839ba3c4a4fdfd40e2e7f039e2cd9d77d3539
Maryam Bano <maryambano.official@gmail.com>
2026-01-06 20:36:58 +0500

    Add Fibonacci series generator using function
    Implements a program that generates the Fibonacci series up to n terms using a function. The user is prompted to enter the number of terms, and the series is printed accordingly.

---
+++ Self-Learning-Data-Analytics/Python/Day 6/04-question4.py
@@ -0,0 +1,10 @@
+# Program to generate Fibonacci series up to n terms using a function
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter number of terms: "))
+fibonacci(n)
